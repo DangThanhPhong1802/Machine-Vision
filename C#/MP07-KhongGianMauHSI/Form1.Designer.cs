752c2490f06027f760dd551835e5f4c8efb6883a
Đặng Thanh Phong <113707377+DangThanhPhong1802@users.noreply.github.com>
2023-03-12 23:20:16 +0700

    Add files via upload

---
+++ C#/MP07-KhongGianMauHSI/Form1.Designer.cs
@@ -0,0 +1,192 @@
+﻿namespace MP07_KhongGianMauHSI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.img1_RGB = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.img2_HUE = new System.Windows.Forms.PictureBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.img3_SATURATION = new System.Windows.Forms.PictureBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.img4_INTENSITY = new System.Windows.Forms.PictureBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.img5_HSI = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.img1_RGB)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.img2_HUE)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.img3_SATURATION)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.img4_INTENSITY)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.img5_HSI)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(437, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(266, 23);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Đặng Thanh Phong - 20146132";
+            // 
+            // img1_RGB
+            // 
+            this.img1_RGB.Location = new System.Drawing.Point(55, 230);
+            this.img1_RGB.Name = "img1_RGB";
+            this.img1_RGB.Size = new System.Drawing.Size(300, 300);
+            this.img1_RGB.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.img1_RGB.TabIndex = 1;
+            this.img1_RGB.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(55, 194);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(77, 16);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "RGB Image";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(441, 52);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(77, 16);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "HUE Image";
+            // 
+            // img2_HUE
+            // 
+            this.img2_HUE.Location = new System.Drawing.Point(441, 88);
+            this.img2_HUE.Name = "img2_HUE";
+            this.img2_HUE.Size = new System.Drawing.Size(300, 300);
+            this.img2_HUE.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.img2_HUE.TabIndex = 3;
+            this.img2_HUE.TabStop = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(817, 52);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(136, 16);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "SATURATION Image";
+            // 
+            // img3_SATURATION
+            // 
+            this.img3_SATURATION.Location = new System.Drawing.Point(817, 88);
+            this.img3_SATURATION.Name = "img3_SATURATION";
+            this.img3_SATURATION.Size = new System.Drawing.Size(300, 300);
+            this.img3_SATURATION.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.img3_SATURATION.TabIndex = 5;
+            this.img3_SATURATION.TabStop = false;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(441, 423);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(119, 16);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "INTENSITY Image";
+            // 
+            // img4_INTENSITY
+            // 
+            this.img4_INTENSITY.Location = new System.Drawing.Point(441, 459);
+            this.img4_INTENSITY.Name = "img4_INTENSITY";
+            this.img4_INTENSITY.Size = new System.Drawing.Size(300, 300);
+            this.img4_INTENSITY.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.img4_INTENSITY.TabIndex = 7;
+            this.img4_INTENSITY.TabStop = false;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(817, 423);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(70, 16);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "HSI Image";
+            // 
+            // img5_HSI
+            // 
+            this.img5_HSI.Location = new System.Drawing.Point(817, 459);
+            this.img5_HSI.Name = "img5_HSI";
+            this.img5_HSI.Size = new System.Drawing.Size(300, 300);
+            this.img5_HSI.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.img5_HSI.TabIndex = 9;
+            this.img5_HSI.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1182, 853);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.img5_HSI);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.img4_INTENSITY);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.img3_SATURATION);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.img2_HUE);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.img1_RGB);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "KHONG GIAN MAU HSI";
+            ((System.ComponentModel.ISupportInitialize)(this.img1_RGB)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.img2_HUE)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.img3_SATURATION)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.img4_INTENSITY)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.img5_HSI)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox img1_RGB;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.PictureBox img2_HUE;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.PictureBox img3_SATURATION;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.PictureBox img4_INTENSITY;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.PictureBox img5_HSI;
+    }
+}
+
---
+++ C#/MP07-KhongGianMauHSI/Form1.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MP07_KhongGianMauHSI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            //Load và hiển thị hình gốc lên img1_RGB 
+            Bitmap Original = new Bitmap(@"D:\Visual Studio 2022\Source\Nhom_07_BPT_C_Sharp\lena_color_resize.jpg");
+            img1_RGB.Image = Original;
+
+            List<Bitmap> HSI = Convert_HSI(Original);
+
+            img2_HUE.Image = HSI[0];
+            img3_SATURATION.Image = HSI[1];
+            img4_INTENSITY.Image = HSI[2];
+            img5_HSI.Image = HSI[3];
+
+        }
+
+        public List<Bitmap> Convert_HSI(Bitmap Original)
+        {
+            //Tạo mảng động LIST chứa các KQ sau khi chuyển đổi
+            List<Bitmap> HSI = new List<Bitmap>();
+
+            //Tạo 3 kênh màu chứa hình các kênh H-S-I
+            Bitmap Hue = new Bitmap(Original.Width, Original.Height);
+            Bitmap Saturation = new Bitmap(Original.Width, Original.Height);
+            Bitmap Intensity = new Bitmap(Original.Width, Original.Height);
+
+            //Tạo hình HSI = kết hợp 3 kênh H-S-I
+            Bitmap HSI_IMG = new Bitmap(Original.Width, Original.Height);
+
+            //Quét từng điểm ảnh có trong hình và quét cột theo cột
+            for (int x=0; x<Original.Width; x++)
+                for (int y=0; y<Original.Height; y++)
+                {
+                    //Lấy thông tin điểm ảnh tại vị trí (x,y)
+                    Color pixel = Original.GetPixel(x, y);
+
+                    //Ở các dạng trước ta dùng kiểu Byte cho R-G-B
+                    //Tuy nhiên, do quá trình tính toán HSI thì KQ trả về là kiểu Double (số thực) nên ta dùng kiểu Double
+                    double R = pixel.R;
+                    double G = pixel.G;
+                    double B = pixel.B;
+
+                    /*=====================================TÍNH GÓC THETA=====================================*/
+                    //Tử số
+                    double t1 = ((R-G)+(R-B))/2;
+                    //Mẫu số
+                    double t2 = Math.Pow((R-G)*(R-G)+(R-B)*(G-B),0.5);
+                    //Theta
+                    //Lưu ý : theo công thức là Độ. Nhưng trong C#.NET là Radian
+                    double theta = Math.Acos(t1/t2);
+
+                    /*=====================================TÍNH GIÁ TRỊ HUE=====================================*/
+                    double H = 0;
+                    //TH1 : Nếu Blue <= Green thì Hue = theta
+                    if (B <= G) 
+                    {
+                        H = theta;
+
+                    }    
+                    //TH2 : Nếu Blue > Green thì Hue được tính như sau : 
+                    else 
+                    {
+                        H = 2*Math.PI-theta;
+                        //Đổi ra Độ
+                        H = (H*180)/Math.PI;
+
+                    }
+
+                    /*=====================================TÍNH GIÁ TRỊ SATURATION=====================================*/
+                    double S = 1 - 3*Math.Min(R, Math.Min(G,B))/(R+G+B);
+
+                    /*=====================================TÍNH GIÁ TRỊ INTENSITY=====================================*/
+                    //Nhìn tương tự như RGB sang GrayScale
+                    double I = (R+G+B)/3;
+
+
+
+
+                    //Hiển thị các kênh giá trị H-S-I
+                    //Lưu ý : phải ép kiểu của H-S-I về kiểu Byte thì Bitmap mới hiểu và hiện thị được
+                    //Do gt tính của S nằm trong [0,1].
+                    //Để Bitmap hiển thị được hình thì phải chuyển S sang gt [0,255] => S = S*255
+                    Hue.SetPixel(x, y, Color.FromArgb((byte)H,(byte)H,(byte)H));
+                    Saturation.SetPixel(x, y, Color.FromArgb((byte)(S*255), (byte)(S*255), (byte)(S*255)));
+                    Intensity.SetPixel(x, y, Color.FromArgb((byte)I, (byte)I, (byte)I));
+
+                    //Hiển thị kênh giá trị tổng hợp HSI
+                    HSI_IMG.SetPixel(x, y, Color.FromArgb((byte)H, (byte)(S*255), (byte)I));
+
+                }
+
+            HSI.Add(Hue);
+            HSI.Add(Saturation);
+            HSI.Add(Intensity);
+            HSI.Add(HSI_IMG);
+
+            return HSI;
+
+        }
+    }
+}
