6952f740aa5ed68d7a7a46e0f03edfea6402cca0
Đặng Thanh Phong <113707377+DangThanhPhong1802@users.noreply.github.com>
2023-03-12 23:24:12 +0700

    Add files via upload

---
+++ C#/MP10-KhongGianMauYCrCb/Form1.Designer.cs
@@ -0,0 +1,197 @@
+﻿namespace MP10_KhongGianMauYCrCb
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.img1_RGB = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.img2_Y = new System.Windows.Forms.PictureBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.img3_Cr = new System.Windows.Forms.PictureBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.img4_Cb = new System.Windows.Forms.PictureBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.img5_YCrCb = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.img1_RGB)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.img2_Y)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.img3_Cr)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.img4_Cb)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.img5_YCrCb)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(454, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(266, 23);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Đặng Thanh Phong - 20146132";
+            // 
+            // img1_RGB
+            // 
+            this.img1_RGB.Location = new System.Drawing.Point(86, 261);
+            this.img1_RGB.Name = "img1_RGB";
+            this.img1_RGB.Size = new System.Drawing.Size(300, 300);
+            this.img1_RGB.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.img1_RGB.TabIndex = 1;
+            this.img1_RGB.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(86, 221);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(110, 23);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "RGB Image";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(458, 52);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(79, 23);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Y Image";
+            // 
+            // img2_Y
+            // 
+            this.img2_Y.Location = new System.Drawing.Point(458, 92);
+            this.img2_Y.Name = "img2_Y";
+            this.img2_Y.Size = new System.Drawing.Size(300, 300);
+            this.img2_Y.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.img2_Y.TabIndex = 3;
+            this.img2_Y.TabStop = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(820, 52);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(89, 23);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Cr Image";
+            // 
+            // img3_Cr
+            // 
+            this.img3_Cr.Location = new System.Drawing.Point(820, 92);
+            this.img3_Cr.Name = "img3_Cr";
+            this.img3_Cr.Size = new System.Drawing.Size(300, 300);
+            this.img3_Cr.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.img3_Cr.TabIndex = 5;
+            this.img3_Cr.TabStop = false;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(458, 412);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(91, 23);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Cb Image";
+            // 
+            // img4_Cb
+            // 
+            this.img4_Cb.Location = new System.Drawing.Point(458, 452);
+            this.img4_Cb.Name = "img4_Cb";
+            this.img4_Cb.Size = new System.Drawing.Size(300, 300);
+            this.img4_Cb.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.img4_Cb.TabIndex = 7;
+            this.img4_Cb.TabStop = false;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(820, 412);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(126, 23);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "YCrCb Image";
+            // 
+            // img5_YCrCb
+            // 
+            this.img5_YCrCb.Location = new System.Drawing.Point(820, 452);
+            this.img5_YCrCb.Name = "img5_YCrCb";
+            this.img5_YCrCb.Size = new System.Drawing.Size(300, 300);
+            this.img5_YCrCb.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.img5_YCrCb.TabIndex = 9;
+            this.img5_YCrCb.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1182, 853);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.img5_YCrCb);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.img4_Cb);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.img3_Cr);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.img2_Y);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.img1_RGB);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "KHONG GIAN MAU YCRCB";
+            ((System.ComponentModel.ISupportInitialize)(this.img1_RGB)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.img2_Y)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.img3_Cr)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.img4_Cb)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.img5_YCrCb)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox img1_RGB;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.PictureBox img2_Y;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.PictureBox img3_Cr;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.PictureBox img4_Cb;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.PictureBox img5_YCrCb;
+    }
+}
+
