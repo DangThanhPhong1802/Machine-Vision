deb9b0f74fb88f4e64dfa9011901369a52836c78
Đặng Thanh Phong <113707377+DangThanhPhong1802@users.noreply.github.com>
2023-03-12 23:22:57 +0700

    Add files via upload

---
+++ C#/MP09-KhongGianMauXYZ/Form1.Designer.cs
@@ -0,0 +1,197 @@
+﻿namespace MP09_KhongGianMauXYZ
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.img1_RGB = new System.Windows.Forms.PictureBox();
+            this.img2_X = new System.Windows.Forms.PictureBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.img3_Y = new System.Windows.Forms.PictureBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.img4_Z = new System.Windows.Forms.PictureBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.img5_XYZ = new System.Windows.Forms.PictureBox();
+            this.label6 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.img1_RGB)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.img2_X)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.img3_Y)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.img4_Z)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.img5_XYZ)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(433, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(266, 23);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Đặng Thanh Phong - 20146132";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(60, 199);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(110, 23);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "RGB Image";
+            // 
+            // img1_RGB
+            // 
+            this.img1_RGB.Location = new System.Drawing.Point(63, 236);
+            this.img1_RGB.Name = "img1_RGB";
+            this.img1_RGB.Size = new System.Drawing.Size(300, 300);
+            this.img1_RGB.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.img1_RGB.TabIndex = 2;
+            this.img1_RGB.TabStop = false;
+            // 
+            // img2_X
+            // 
+            this.img2_X.Location = new System.Drawing.Point(437, 78);
+            this.img2_X.Name = "img2_X";
+            this.img2_X.Size = new System.Drawing.Size(300, 300);
+            this.img2_X.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.img2_X.TabIndex = 4;
+            this.img2_X.TabStop = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(434, 41);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(80, 23);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "X Image";
+            // 
+            // img3_Y
+            // 
+            this.img3_Y.Location = new System.Drawing.Point(811, 78);
+            this.img3_Y.Name = "img3_Y";
+            this.img3_Y.Size = new System.Drawing.Size(300, 300);
+            this.img3_Y.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.img3_Y.TabIndex = 6;
+            this.img3_Y.TabStop = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(808, 41);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(79, 23);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Y Image";
+            // 
+            // img4_Z
+            // 
+            this.img4_Z.Location = new System.Drawing.Point(437, 438);
+            this.img4_Z.Name = "img4_Z";
+            this.img4_Z.Size = new System.Drawing.Size(300, 300);
+            this.img4_Z.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.img4_Z.TabIndex = 8;
+            this.img4_Z.TabStop = false;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(434, 401);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(79, 23);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Z Image";
+            // 
+            // img5_XYZ
+            // 
+            this.img5_XYZ.Location = new System.Drawing.Point(811, 438);
+            this.img5_XYZ.Name = "img5_XYZ";
+            this.img5_XYZ.Size = new System.Drawing.Size(300, 300);
+            this.img5_XYZ.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.img5_XYZ.TabIndex = 10;
+            this.img5_XYZ.TabStop = false;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(808, 401);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(105, 23);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "XYZ Image";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1182, 853);
+            this.Controls.Add(this.img5_XYZ);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.img4_Z);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.img3_Y);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.img2_X);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.img1_RGB);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "KHONG GIAN MAU XYZ";
+            ((System.ComponentModel.ISupportInitialize)(this.img1_RGB)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.img2_X)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.img3_Y)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.img4_Z)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.img5_XYZ)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox img1_RGB;
+        private System.Windows.Forms.PictureBox img2_X;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.PictureBox img3_Y;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.PictureBox img4_Z;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.PictureBox img5_XYZ;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ C#/MP09-KhongGianMauXYZ/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MP09_KhongGianMauXYZ
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+
+            //Load và hiển thị hình gốc lên img1_RGB 
+            Bitmap Original = new Bitmap(@"D:\Visual Studio 2022\Source\Nhom_07_BPT_C_Sharp\lena_color_resize.jpg");
+            img1_RGB.Image = Original;
+
+            List<Bitmap> XYZ = Convert_XYZ(Original);
+
+            img2_X.Image = XYZ[0];
+            img3_Y.Image = XYZ[1];
+            img4_Z.Image = XYZ[2];
+            img5_XYZ.Image = XYZ[3];
+        }
+
+
+        public List<Bitmap> Convert_XYZ(Bitmap Original)
+        {
+            //Tạo mảng động LIST chứa các KQ sau khi chuyển đổi
+            List<Bitmap> XYZ = new List<Bitmap>();
+
+            //Tạo 3 kênh màu chứa hình các kênh X-Y-Z
+            Bitmap XIMG = new Bitmap(Original.Width, Original.Height);
+            Bitmap YIMG = new Bitmap(Original.Width, Original.Height);
+            Bitmap ZIMG = new Bitmap(Original.Width, Original.Height);
+
+            //Tạo hình XYZ = kết hợp 3 kênh X-Y-Z
+            Bitmap XYZ_IMG = new Bitmap(Original.Width, Original.Height);
+
+            //Quét từng điểm ảnh có trong hình và quét cột theo cột
+            for (int x = 0; x < Original.Width; x++)
+                for (int y = 0; y < Original.Height; y++)
+                {
+                    //Lấy thông tin điểm ảnh tại vị trí (x,y)
+                    Color pixel = Original.GetPixel(x, y);
+
+                    //Ở các dạng trước ta dùng kiểu Byte cho R-G-B
+                    //Tuy nhiên, do quá trình tính toán XYZ thì KQ trả về là kiểu Double (số thực) nên ta dùng kiểu Double
+                    double R = pixel.R;
+                    double G = pixel.G;
+                    double B = pixel.B;
+
+                    //Tính X-Y-Z
+                    double X = (0.4124564 * R + 0.3575761 * G + 0.1804375 * B);
+                    double Y = (0.2126729 * R + 0.7151522 * G + 0.0721750 * B);
+                    double Z = (0.0193339 * R + 0.1191920 * G + 0.9503041 * B);
+                    
+
+                    //Hiển thị các kênh giá trị X-Y-Z
+                    //Lưu ý : phải ép kiểu của X-Y-Z về kiểu Byte thì Bitmap mới hiểu và hiện thị được
+                    XIMG.SetPixel(x, y, Color.FromArgb((byte)X, (byte)X, (byte)X));
+                    YIMG.SetPixel(x, y, Color.FromArgb((byte)Y, (byte)Y, (byte)Y));
+                    ZIMG.SetPixel(x, y, Color.FromArgb((byte)Z, (byte)Z, (byte)Z));
+
+                    //Hiển thị kênh giá trị tổng hợp XYZ
+                    XYZ_IMG.SetPixel(x, y, Color.FromArgb((byte)X, (byte)Y, (byte)Z));
+
+                }
+
+            XYZ.Add(XIMG);
+            XYZ.Add(YIMG);
+            XYZ.Add(ZIMG);
+            XYZ.Add(XYZ_IMG);
+
+            return XYZ;
+
+        }
+    }
+}
