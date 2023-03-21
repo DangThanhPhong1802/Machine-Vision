4df6ead033e38ea33a3757a909a59ee402214138
Đặng Thanh Phong <113707377+DangThanhPhong1802@users.noreply.github.com>
2023-03-21 22:57:37 +0700

    Add files via upload

---
+++ C#/MP11-LamMuotAnhMau/Form1.Designer.cs
@@ -0,0 +1,192 @@
+﻿namespace MP11_LamMuotAnhMau
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.img1_RGB = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.img2_3x3 = new System.Windows.Forms.PictureBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.img3_5x5 = new System.Windows.Forms.PictureBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.img4_7x7 = new System.Windows.Forms.PictureBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.img5_9x9 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.img1_RGB)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.img2_3x3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.img3_5x5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.img4_7x7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.img5_9x9)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(482, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(266, 23);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Đặng Thanh Phong - 20146132";
+            // 
+            // img1_RGB
+            // 
+            this.img1_RGB.Location = new System.Drawing.Point(54, 294);
+            this.img1_RGB.Name = "img1_RGB";
+            this.img1_RGB.Size = new System.Drawing.Size(300, 300);
+            this.img1_RGB.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.img1_RGB.TabIndex = 1;
+            this.img1_RGB.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(54, 258);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(77, 16);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "RGB Image";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(448, 54);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(142, 16);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Smoothing - 3x3 Image";
+            // 
+            // img2_3x3
+            // 
+            this.img2_3x3.Location = new System.Drawing.Point(448, 90);
+            this.img2_3x3.Name = "img2_3x3";
+            this.img2_3x3.Size = new System.Drawing.Size(300, 300);
+            this.img2_3x3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.img2_3x3.TabIndex = 3;
+            this.img2_3x3.TabStop = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(810, 54);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(142, 16);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Smoothing - 5x5 Image";
+            // 
+            // img3_5x5
+            // 
+            this.img3_5x5.Location = new System.Drawing.Point(810, 90);
+            this.img3_5x5.Name = "img3_5x5";
+            this.img3_5x5.Size = new System.Drawing.Size(300, 300);
+            this.img3_5x5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.img3_5x5.TabIndex = 5;
+            this.img3_5x5.TabStop = false;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(448, 439);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(142, 16);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Smoothing - 7x7 Image";
+            // 
+            // img4_7x7
+            // 
+            this.img4_7x7.Location = new System.Drawing.Point(448, 472);
+            this.img4_7x7.Name = "img4_7x7";
+            this.img4_7x7.Size = new System.Drawing.Size(300, 300);
+            this.img4_7x7.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.img4_7x7.TabIndex = 7;
+            this.img4_7x7.TabStop = false;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(810, 436);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(142, 16);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Smoothing - 9x9 Image";
+            // 
+            // img5_9x9
+            // 
+            this.img5_9x9.Location = new System.Drawing.Point(810, 472);
+            this.img5_9x9.Name = "img5_9x9";
+            this.img5_9x9.Size = new System.Drawing.Size(300, 300);
+            this.img5_9x9.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.img5_9x9.TabIndex = 9;
+            this.img5_9x9.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1182, 853);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.img5_9x9);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.img4_7x7);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.img3_5x5);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.img2_3x3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.img1_RGB);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "LAM MUOT ANH - SMOOTHING";
+            ((System.ComponentModel.ISupportInitialize)(this.img1_RGB)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.img2_3x3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.img3_5x5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.img4_7x7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.img5_9x9)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox img1_RGB;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.PictureBox img2_3x3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.PictureBox img3_5x5;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.PictureBox img4_7x7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.PictureBox img5_9x9;
+    }
+}
+
