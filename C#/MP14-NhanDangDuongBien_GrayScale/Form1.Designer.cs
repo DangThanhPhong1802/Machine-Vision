ed2943e29b296a727463b12ed232b55f6f87e637
Đặng Thanh Phong <113707377+DangThanhPhong1802@users.noreply.github.com>
2023-03-21 23:01:23 +0700

    Add files via upload

---
+++ C#/MP14-NhanDangDuongBien_GrayScale/Form1.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace MP14_NhanDangDuongBien_GrayScale
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.img1_Original = new System.Windows.Forms.PictureBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.img2_Sobel = new System.Windows.Forms.PictureBox();
+            this.scroll_Nguong = new System.Windows.Forms.HScrollBar();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lb_Nguong = new System.Windows.Forms.Label();
+            this.btn_RUN = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.img1_Original)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.img2_Sobel)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(460, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(266, 23);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Đặng Thanh Phong - 20146132";
+            // 
+            // img1_Original
+            // 
+            this.img1_Original.Location = new System.Drawing.Point(50, 86);
+            this.img1_Original.Name = "img1_Original";
+            this.img1_Original.Size = new System.Drawing.Size(512, 512);
+            this.img1_Original.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.img1_Original.TabIndex = 1;
+            this.img1_Original.TabStop = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(599, 45);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(369, 23);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Edge Detection - Sobel - Luminance Image";
+            // 
+            // img2_Sobel
+            // 
+            this.img2_Sobel.Location = new System.Drawing.Point(603, 86);
+            this.img2_Sobel.Name = "img2_Sobel";
+            this.img2_Sobel.Size = new System.Drawing.Size(512, 512);
+            this.img2_Sobel.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.img2_Sobel.TabIndex = 3;
+            this.img2_Sobel.TabStop = false;
+            // 
+            // scroll_Nguong
+            // 
+            this.scroll_Nguong.Location = new System.Drawing.Point(50, 675);
+            this.scroll_Nguong.Maximum = 255;
+            this.scroll_Nguong.Name = "scroll_Nguong";
+            this.scroll_Nguong.Size = new System.Drawing.Size(1065, 35);
+            this.scroll_Nguong.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(46, 45);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(136, 23);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Original Image";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(46, 639);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(111, 23);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Threshold : ";
+            // 
+            // lb_Nguong
+            // 
+            this.lb_Nguong.AutoSize = true;
+            this.lb_Nguong.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lb_Nguong.Location = new System.Drawing.Point(163, 639);
+            this.lb_Nguong.Name = "lb_Nguong";
+            this.lb_Nguong.Size = new System.Drawing.Size(0, 23);
+            this.lb_Nguong.TabIndex = 8;
+            // 
+            // btn_RUN
+            // 
+            this.btn_RUN.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_RUN.Location = new System.Drawing.Point(493, 731);
+            this.btn_RUN.Name = "btn_RUN";
+            this.btn_RUN.Size = new System.Drawing.Size(174, 110);
+            this.btn_RUN.TabIndex = 9;
+            this.btn_RUN.Text = "Edge Detection";
+            this.btn_RUN.UseVisualStyleBackColor = true;
+            this.btn_RUN.Click += new System.EventHandler(this.btn_RUN_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1182, 853);
+            this.Controls.Add(this.btn_RUN);
+            this.Controls.Add(this.lb_Nguong);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.scroll_Nguong);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.img2_Sobel);
+            this.Controls.Add(this.img1_Original);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "NHAN DANG DUONG BIEN - SOBEL - GRAYSCALE";
+            ((System.ComponentModel.ISupportInitialize)(this.img1_Original)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.img2_Sobel)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox img1_Original;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.PictureBox img2_Sobel;
+        private System.Windows.Forms.HScrollBar scroll_Nguong;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lb_Nguong;
+        private System.Windows.Forms.Button btn_RUN;
+    }
+}
+
---
+++ C#/MP14-NhanDangDuongBien_GrayScale/Form1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MP14_NhanDangDuongBien_GrayScale
+{
+    public partial class Form1 : Form
+    {
+        //Load hình gốc cô gái Lena
+        Bitmap Original = new Bitmap(@"D:\Visual Studio 2022\Source\Nhom_07_BPT_C_Sharp\lena_color.jpg");
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            //Hiển thị hình gốc lên im1_Original
+            img1_Original.Image = Original;
+        }
+
+        public Bitmap LuminanceIMG(Bitmap Original)
+        {
+            Bitmap GrayScale = new Bitmap(Original.Width, Original.Height);
+            for (int x = 0; x < Original.Width; x++)
+                for (int y = 0; y < Original.Height; y++)
+                {
+                    //Lấy điểm ảnh tại (x,y)
+                    Color pixel = Original.GetPixel(x, y);
+                    byte R = pixel.R;
+                    byte G = pixel.G;
+                    byte B = pixel.B;
+
+                    //Giá trị mức xám điểm ảnh tại (x,y)
+                    byte Gray = (byte)(0.2126 * R + 0.7152 * G + 0.0722 * B);
+
+                    //Gán gray vào hình mức xám
+                    GrayScale.SetPixel(x, y, Color.FromArgb(Gray, Gray, Gray));
+
+                }
+            return GrayScale;
+        }
+
+        public Bitmap SobelIMG(Bitmap Hinhxam, int Nguong)
+        {
+            int[,] MTSobelx =
+            {
+                {-1, -2, -1},
+                {0, 0, 0},
+                {1, 2, 1 },
+            };
+            int[,] MTSobely =
+            {
+                {-1, 0, 1 },
+                {-2, 0, 2 },
+                {-1, 0, 1 },
+            };
+            Bitmap AnhDuongBien = new Bitmap(Hinhxam.Width, Hinhxam.Height);
+            for (int x = 1; x < Hinhxam.Width - 1; x++)
+                for (int y = 1; y < Hinhxam.Height - 1; y++)
+                {
+                    int gx = 0, gy = 0;
+                    for (int i = x - 1; i <= x + 1; i++)
+                        for (int j = y - 1; j <= y + 1; j++)
+                        {
+                            Color color = Hinhxam.GetPixel(i, j);
+                            int gR = color.R;
+                            gx += gR * MTSobelx[i - x + 1, j - y + 1];
+                            gy += gR * MTSobely[i - x + 1, j - y + 1];
+
+                        }
+                    int M = Math.Abs(gx) + Math.Abs(gy);
+                    if (M < Nguong)
+                        AnhDuongBien.SetPixel(x, y, Color.FromArgb(0, 0, 0));
+                    else
+                        AnhDuongBien.SetPixel(x, y, Color.FromArgb(255, 255, 255));
+                }
+            return AnhDuongBien;
+        }
+        private void btn_RUN_Click(object sender, EventArgs e)
+        {
+            //Lấy GT ngưỡng từ thanh cuộn
+            //Do value của thanh cuộn là kiểu int, trong khi ngưỡng là kiểu Byte
+            //Do đó phải chuyển từ Int về Byte
+            byte Nguong = (byte)scroll_Nguong.Value;
+
+            //Hiển thị Gt ngưỡng
+            lb_Nguong.Text = Nguong.ToString();
+            Bitmap GrayScale = LuminanceIMG(Original);
+            Bitmap AnhDuongBien = SobelIMG(GrayScale, Nguong);
+            img2_Sobel.Image = AnhDuongBien;
+        }
+    }
+}
