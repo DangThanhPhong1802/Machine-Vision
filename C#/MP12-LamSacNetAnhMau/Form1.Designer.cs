7563c56f95027ed2c1888f711c6435a389b2eadc
Đặng Thanh Phong <113707377+DangThanhPhong1802@users.noreply.github.com>
2023-03-21 22:58:54 +0700

    Add files via upload

---
+++ C#/MP12-LamSacNetAnhMau/Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace MP12_LamSacNetAnhMau
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.img1_Original = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.img2_Sharpen = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.img1_Original)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.img2_Sharpen)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(459, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(266, 23);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Đặng Thanh Phong - 20146132";
+            // 
+            // img1_Original
+            // 
+            this.img1_Original.Location = new System.Drawing.Point(58, 117);
+            this.img1_Original.Name = "img1_Original";
+            this.img1_Original.Size = new System.Drawing.Size(512, 512);
+            this.img1_Original.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.img1_Original.TabIndex = 1;
+            this.img1_Original.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(58, 76);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(94, 16);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Original Image";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(616, 76);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(122, 16);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Sharpened - Image";
+            // 
+            // img2_Sharpen
+            // 
+            this.img2_Sharpen.Location = new System.Drawing.Point(616, 117);
+            this.img2_Sharpen.Name = "img2_Sharpen";
+            this.img2_Sharpen.Size = new System.Drawing.Size(512, 512);
+            this.img2_Sharpen.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.img2_Sharpen.TabIndex = 3;
+            this.img2_Sharpen.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1182, 753);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.img2_Sharpen);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.img1_Original);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "LAM SAC NET ANH MAU";
+            ((System.ComponentModel.ISupportInitialize)(this.img1_Original)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.img2_Sharpen)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox img1_Original;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.PictureBox img2_Sharpen;
+    }
+}
+
---
+++ C#/MP12-LamSacNetAnhMau/Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MP12_LamSacNetAnhMau
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            //Load và hiển thị hình gốc cô gái Lena
+            Bitmap Original = new Bitmap(@"D:\Visual Studio 2022\Source\Nhom_07_BPT_C_Sharp\lena_color.jpg");
+            img1_Original.Image = Original;
+
+            //Tạo biến chứa hình sắc nét sau đó cho hiển thị
+            Bitmap SharpenIMG = Sharpen_3x3(Original);
+            img2_Sharpen.Image = SharpenIMG;
+        }
+
+        public Bitmap Sharpen_3x3(Bitmap Original)
+        {
+            int[,] matrix = { {0,-1,0}, {-1,4,-1}, {0,-1,0} };
+
+            //Tạo sẵn 1 hình Bitmap để chứa ảnh được làm mượt
+            Bitmap SharpenIMG = new Bitmap(Original.Width, Original.Height);
+
+            //Quét các điểm ảnh có trong hình gốc RGB
+            //Để dễ lập trình, ta bỏ qua viền ngoài của ảnh
+            //Với Mask 3x3 : chỉ tính từ x=1 tới x=width-1 ; y=1 tới y=height-1
+            for (int x = 1; x < Original.Width - 1; x++)
+                for (int y = 1; y < Original.Height - 1; y++)
+                {
+                    //Các biến này chứa các giá trị cộng dồn của điểm ảnh nằm trong mặt nạ
+                    //Khai báo các biến sau kiểu int để có thể đọc được giá trị cộng dồn của các pixel
+                    int Rs = 0, Gs = 0, Bs = 0;
+                    int ShR = 0, ShG = 0, ShB = 0;
+
+                    //Quét các điểm trong mặt nạ
+                    for (int i = x - 1; i <= x + 1; i++)
+                        for (int j = y - 1; j <= y + 1; j++)
+                        {
+                            //Lấy thông tin màu R-G-B tại điểm ảnh có trong mặt nạ tại vị trí (i,j)
+                            Color color = Original.GetPixel(i, j);
+                            byte R = color.R;
+                            byte G = color.G;
+                            byte B = color.B;
+
+                            //Nhân tích chập tất cả các điểm ảnh đó cho mỗi kênh R-G-B tương ứng
+                            Rs += R * matrix[i - x + 1, j - y + 1];
+                            Gs += G * matrix[i - x + 1, j - y + 1];
+                            Bs += B * matrix[i - x + 1, j - y + 1];
+                        }
+                    //Kết thúc quét và cộng dồn
+                    //Tính điểm làm sắc nét mỗi kênh theo CT 3.6-7 ứng với từng kênh R-G-B
+                    Color color1 = Original.GetPixel(x, y);
+                    int R1 = color1.R;
+                    int G1 = color1.G;
+                    int B1 = color1.B;
+
+                    ShR = R1 + Rs;
+                    ShG = G1 + Gs;
+                    ShB = B1 + Bs;
+
+                    if (ShR < 0)
+                        ShR = 0;
+                    else if (ShR > 255)
+                        ShR = 255;
+
+                    if (ShG < 0)
+                        ShG = 0;
+                    else if (ShG > 255)
+                        ShG = 255;
+
+                    if (ShB < 0)
+                        ShB = 0;
+                    else if (ShB > 255)
+                        ShB = 255;
+
+                    //Set điểm ảnh đã làm mượt (mờ) vào ảnh Bitmap
+                    SharpenIMG.SetPixel(x, y, Color.FromArgb(ShR,ShG,ShB));
+                }
+            return SharpenIMG;
+        }
+    }
+}
